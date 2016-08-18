9b69058df9cd9a08a219fbb4493f3c5c68d5502e
Shivam Kumar Singh <shivam.kumar@girmiti.com>
2016-08-18 09:43:46 +0530

    this is a sample application of windows service.

---
+++ myservice/Library.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace myservice
+{
+   public static class Library
+    {
+        public static void WriteErrorLog(Exception ex)
+        {
+            StreamWriter sw = null;
+            try
+            {
+            sw = new StreamWriter("F:\\LogFile.txt", true);
+                sw.WriteLine(DateTime.Now.ToString() + ":" + ex.Source.ToString().Trim() + ";" + ex.Message.ToString().Trim());
+                sw.Flush();
+                sw.Close(); 
+            }
+            catch
+            {
+
+            }
+        }
+        public static void WriteErrorLog(String message)
+        {
+            StreamWriter sw = null;
+            try
+            {
+              sw = new StreamWriter( "F:\\LogFile.txt", true);
+                sw.WriteLine(DateTime.Now.ToString() + ":" +message);
+                sw.Flush();
+                sw.Close(); 
+            }
+            catch
+            {
+
+            }
+        }
+    }
+}
---
+++ myservice/Scheduler.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace myservice
+{
+    public partial class Scheduler : ServiceBase
+    {
+        public int i;
+        private Timer timer1 = null;
+        public Scheduler()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+           timer1 = new Timer();
+            this.timer1.Interval = 1;
+            this.timer1.Elapsed += timer1_tick;
+            timer1.Enabled = true;
+            i = 1;
+            Library.WriteErrorLog("The timer been started");
+        }
+
+        private void timer1_tick(object sender, ElapsedEventArgs e)
+        {
+            
+            Library.WriteErrorLog(String.Format("Task {0} has been completed", i));
+            i++;
+        }
+        
+
+        protected override void OnStop()
+        {
+            timer1.Enabled = false;
+            Library.WriteErrorLog("Window Service has been Stopped");
+           
+        }
+    }
+}
